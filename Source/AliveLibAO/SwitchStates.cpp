e97425939899082b3bc022f924cd63f54766fe08
Léonard <hobbax3@gmail.com>
2021-05-23 23:24:38 +0200

    Begin adapting AO's code to the new types.

--- Source/AliveLibAO/SwitchStates.cpp
+++ Source/AliveLibAO/SwitchStates.cpp
@@ -2,80 +2,14 @@
 #include "SwitchStates.hpp"
 #include "Function.hpp"
 
+
 namespace AO {
 
 ALIVE_VAR(1, 0x505568, SwitchStates, sSwitchStates_505568, {});
 
-void SwitchStates_Set(u16 idx, s8 value)
-{
-    sSwitchStates_505568.mData[idx] = value;
-}
-
-s16 SwitchStates_Get(u16 idx)
-{
-    if (idx == 0)
-    {
-        return 0;
-    }
-
-    if (idx == 1)
-    {
-        return 1;
-    }
-
-    return sSwitchStates_505568.mData[idx];
-}
-
-void SwitchStates_Add(u16 idx, s8 value)
-{
-    sSwitchStates_505568.mData[idx] += value;
-}
-
-
-void SwitchStates_ClearAll()
-{
-    sSwitchStates_505568 = {};
-}
-
-EXPORT void CC SwitchStates_Do_Operation_436A10(s16 idx, SwitchOp operation)
-{
-    if (idx >= 2)
-    {
-        switch (operation)
-        {
-            case SwitchOp::eSetTrue_0:
-                SwitchStates_Set(idx, 1);
-                break;
-
-            case SwitchOp::eSetFalse_1:
-                SwitchStates_Set(idx, 0);
-                break;
-
-            case SwitchOp::eToggle_2:
-                if (SwitchStates_Get(idx))
-                {
-                    // Its on, so turn off
-                    SwitchStates_Set(idx, 0);
-                }
-                else
-                {
-                    // Its off, so turn on
-                    SwitchStates_Set(idx, 1);
-                }
-                break;
-
-            case SwitchOp::eIncrement_3:
-                SwitchStates_Add(idx, 1);
-                break;
-
-            case SwitchOp::eDecrement_4:
-                SwitchStates_Add(idx, -1);
-                break;
-
-            default:
-                return;
-        }
-    }
+EXPORT void CC SwitchStates_Do_Operation_436A10(SwitchId idx, SwitchOp operation)
+{   // Intentionally not declared in the header.
+    sSwitchStates_505568.Operation(idx , operation);
 }
 
 } // namespace AO
--- Source/AliveLibAO/SwitchStates.hpp
+++ Source/AliveLibAO/SwitchStates.hpp
@@ -1,30 +1,9 @@
 #pragma once
 
+#include "SwitchStates_common.hpp"
 #include "FunctionFwd.hpp"
 
 namespace AO {
-
-struct SwitchStates final
-{
-    s8 mData[256];
-};
-
-void SwitchStates_Set(u16 idx, s8 value);
-s16 SwitchStates_Get(u16 idx);
-
-enum class SwitchOp : s16
-{
-    eSetTrue_0 = 0,
-    eSetFalse_1 = 1,
-    eToggle_2 = 2,
-    eIncrement_3 = 3,
-    eDecrement_4 = 4,
-};
-
-EXPORT void CC SwitchStates_Do_Operation_436A10(s16 idx, SwitchOp operation);
-
-void SwitchStates_Add(u16 idx, s8 value);
-void SwitchStates_ClearAll();
 
 ALIVE_VAR_EXTERN(SwitchStates, sSwitchStates_505568);
 
